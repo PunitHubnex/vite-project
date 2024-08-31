ad9c14da8bdfe2be7eb22582aa1b687d358ce325
PunitHubnex <punittripathi2111@gmail.com>
2024-08-31 15:35:01 +0530

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Outlet, Link } from 'react-router-dom';
 import Home from './component/Home';
 import About from './component/About';
