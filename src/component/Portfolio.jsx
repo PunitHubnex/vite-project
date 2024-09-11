788d447f03a4611f4ccf37c3f054a53f3b7fae35
PunitHubnex <punittripathi2111@gmail.com>
2024-09-11 16:36:42 +0530

    updat6e

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -1,326 +1,118 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import ReelDisplay from './ReelDisplay';
+import { Pagination } from 'swiper/modules';
+
 
 const Portfolio = () => {
-  return (
-    <div class="cv_main_wrapper">
-      <div className="cv_portfolio_wrapper">
-  <div className="cv_container container-fluid">
-    <div className="row">
-      <div className="col-12">
-        <ul className="cv_port_tab">
-          <li>
-            <a className="active" data-rel="tab-1" href="javascript:void(0)">
-              All
-            </a>
-          </li>
-          <li>
-            <a className="" data-rel="tab-2" href="javascript:void(0)">
-              Website
-            </a>
-          </li>
-          <li>
-            <a className="" data-rel="tab-3" href="javascript:void(0)">
-              Mobile App
-            </a>
-          </li>
-          <li>
-            <a className="" data-rel="tab-4" href="javascript:void(0)">
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a className="" data-rel="tab-5" href="javascript:void(0)">
-              Creative
-            </a>
-          </li>
-        </ul>
-        {/* All Tab */}
-        <div className="cv_tab_pane " id="tab-1" style={{ display: "block" }}>
-          <div className="cv_gallery_wrapper">
-            <div className="cv_gallery_item item-1">
-              <div className="cv_gallery_small">
-                <div className="cv_gallery_img img-1">
-                  <img src="../../images/port-1.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-                <div className="cv_gallery_img img-2">
-                  <img src="../../images/port-2.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-              </div>
-              <div className="cv_gallery_img img-3">
-                <img src="../../images/port-5.webp" className="img-fluid" />
-                <span className="cv_gallery_hover">
-                  <p>Website</p>
-                  <h4>Creative Website</h4>
-                </span>
-              </div>
-            </div>
-            <div className="cv_gallery_item item-2">
-              <div className="cv_gallery_img img-4">
-                <img src="../../images/port-3.webp" className="img-fluid" />
-                <span className="cv_gallery_hover">
-                  <p>Website</p>
-                  <h4>Creative Website</h4>
-                </span>
-              </div>
-              <div className="cv_gallery_small">
-                <div className="cv_gallery_img img-5">
-                  <img src="../../images/port-4.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-                <div className="cv_gallery_img img-6">
-                  <img src="../../images/port-6.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* Website Tab */}
-        <div className="cv_tab_pane" id="tab-2">
-          <div className="cv_gallery_wrapper">
-            <div className="cv_gallery_item item-1">
-              <div className="cv_gallery_small">
-                <div className="cv_gallery_img img-1">
-                  <img src="../../images/port-1.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-                <div className="cv_gallery_img img-2">
-                  <img src="../../images/port-2.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-              </div>
-              <div className="cv_gallery_img img-3">
-                <img src="../../images/port-5.webp" className="img-fluid" />
-                <span className="cv_gallery_hover">
-                  <p>Website</p>
-                  <h4>Creative Website</h4>
-                </span>
-              </div>
-            </div>
-            <div className="cv_gallery_item item-2">
-              <div className="cv_gallery_img img-4">
-                <img src="../../images/port-3.webp" className="img-fluid" />
-                <span className="cv_gallery_hover">
-                  <p>Website</p>
-                  <h4>Creative Website</h4>
-                </span>
-              </div>
-              <div className="cv_gallery_small">
-                <div className="cv_gallery_img img-5">
-                  <img src="../../images/port-4.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-                <div className="cv_gallery_img img-6">
-                  <img src="../../images/port-6.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* Mobile Tab */}
-        <div className="cv_tab_pane" id="tab-3">
-          <div className="cv_gallery_wrapper">
-            <div className="cv_gallery_item item-1">
-              <div className="cv_gallery_small">
-                <div className="cv_gallery_img img-1">
-                  <img src="../../images/port-1.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-                <div className="cv_gallery_img img-2">
-                  <img src="../../images/port-2.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-              </div>
-              <div className="cv_gallery_img img-3">
-                <img src="../../images/port-5.webp" className="img-fluid" />
-                <span className="cv_gallery_hover">
-                  <p>Website</p>
-                  <h4>Creative Website</h4>
-                </span>
-              </div>
-            </div>
-            <div className="cv_gallery_item item-2">
-              <div className="cv_gallery_img img-4">
-                <img src="../../images/port-3.webp" className="img-fluid" />
-                <span className="cv_gallery_hover">
-                  <p>Website</p>
-                  <h4>Creative Website</h4>
-                </span>
-              </div>
-              <div className="cv_gallery_small">
-                <div className="cv_gallery_img img-5">
-                  <img src="../../images/port-4.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-                <div className="cv_gallery_img img-6">
-                  <img src="../../images/port-6.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* Dashboard Tab */}
-        <div className="cv_tab_pane" id="tab-4">
-          <div className="cv_gallery_wrapper">
-            <div className="cv_gallery_item item-1">
-              <div className="cv_gallery_small">
-                <div className="cv_gallery_img img-1">
-                  <img src="../../images/port-1.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-                <div className="cv_gallery_img img-2">
-                  <img src="../../images/port-2.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-              </div>
-              <div className="cv_gallery_img img-3">
-                <img src="../../images/port-5.webp" className="img-fluid" />
-                <span className="cv_gallery_hover">
-                  <p>Website</p>
-                  <h4>Creative Website</h4>
-                </span>
-              </div>
-            </div>
-            <div className="cv_gallery_item item-2">
-              <div className="cv_gallery_img img-4">
-                <img src="../../images/port-3.webp" className="img-fluid" />
-                <span className="cv_gallery_hover">
-                  <p>Website</p>
-                  <h4>Creative Website</h4>
-                </span>
-              </div>
-              <div className="cv_gallery_small">
-                <div className="cv_gallery_img img-5">
-                  <img src="../../images/port-4.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-                <div className="cv_gallery_img img-6">
-                  <img src="../../images/port-6.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* Creative Tab */}
-        <div className="cv_tab_pane" id="tab-5">
-          <div className="cv_gallery_wrapper">
-            <div className="cv_gallery_item item-1">
-              <div className="cv_gallery_small">
-                <div className="cv_gallery_img img-1">
-                  <img src="../../images/port-1.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-                <div className="cv_gallery_img img-2">
-                  <img src="../../images/port-2.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-              </div>
-              <div className="cv_gallery_img img-3">
-                <img src="../../images/port-5.webp" className="img-fluid" />
-                <span className="cv_gallery_hover">
-                  <p>Website</p>
-                  <h4>Creative Website</h4>
-                </span>
-              </div>
-            </div>
-            <div className="cv_gallery_item item-2">
-              <div className="cv_gallery_img img-4">
-                <img src="../../images/port-3.webp" className="img-fluid" />
-                <span className="cv_gallery_hover">
-                  <p>Website</p>
-                  <h4>Creative Website</h4>
-                </span>
-              </div>
-              <div className="cv_gallery_small">
-                <div className="cv_gallery_img img-5">
-                  <img src="../../images/port-4.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-                <div className="cv_gallery_img img-6">
-                  <img src="../../images/port-6.webp" className="img-fluid" />
-                  <span className="cv_gallery_hover">
-                    <p>Website</p>
-                    <h4>Creative Website</h4>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-12">
-        <div className="cv_bottom_btn">
-          <a href="javascript:void(0)" className="cv_btn">
-            View More
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+  const reels = [
+    "../../1.mp4",
+    "../../1.mp4",
+    "../../1.mp4",
+    "../../1.mp4",
+    "../../1.mp4",
+    "../../1.mp4",
+   
+  ];
 
+
+
+  return (
+    <div className="cv_portfolio_wrapper">
+      <div className="cv_container container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <div className="cv_tab_pane" id="tab-1" style={{ display: "block" }}>
+              <h2 className="photoshoots-heading">Photoshoots</h2>
+              <br />
+              <div className="cv_gallery_item item-1">
+                <Swiper
+                  modules={[ Pagination]} // Add navigation and pagination modules
+                  spaceBetween={10} // Space between slides
+                  loop={true} // Enable infinite loop
+                  pagination={true}
+                  breakpoints={{
+                    640: {
+                      slidesPerView: 1, // Mobile view (phones)
+                    },
+                    768: {
+                      slidesPerView: 2, // Tablet view
+                    },
+                    1024: {
+                      slidesPerView: 4, // Desktop view (laptops)
+                    },
+                  }}
+                >
+                  {/* Slider Images */}
+                  <SwiperSlide>
+                    <div className="cv_gallery_img img-2">
+                      <img src="../../Audi.jpg" className="img-fluid" style={{ width: "100%", height: "100%" }} alt="Audi" />
+                      <span className="cv_gallery_hover">
+                        <p>Photo Shoot</p>
+                        <h4>Audi Q8 (2021)</h4>
+                      </span>
+                    </div>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <div className="cv_gallery_img img-2">
+                      <img src="../../cla.jpg" className="img-fluid" style={{ width: "100%", height: "100%" }} alt="Cla" />
+                      <span className="cv_gallery_hover">
+                        <p>Photo Shoot</p>
+                        <h4>Cla 200 (2015)</h4>
+                      </span>
+                    </div>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <div className="cv_gallery_img img-2">
+                      <img src="../../v.jpg" className="img-fluid" style={{ width: "100%", height: "100%" }} alt="Land Cruiser" />
+                      <span className="cv_gallery_hover">
+                        <p>Photo Shoot</p>
+                        <h4>Land Cruiser Vx8</h4>
+                      </span>
+                    </div>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <div className="cv_gallery_img img-2">
+                      <img src="../../w.jpg" className="img-fluid" style={{ width: "100%", height: "100%" }} alt="Velar" />
+                      <span className="cv_gallery_hover">
+                        <p>Photo Shoot</p>
+                        <h4>Velar 2018</h4>
+                      </span>
+                    </div>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <div className="cv_gallery_img img-2">
+                      <img src="../../X5.jpg" className="img-fluid" style={{ width: "100%", height: "100%" }} alt="X5" />
+                      <span className="cv_gallery_hover">
+                        <p>Photo Shoot</p>
+                        <h4>X5</h4>
+                      </span>
+                    </div>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <div className="cv_gallery_img img-2">
+                      <img src="../../Macan.jpg" className="img-fluid" style={{ width: "100%", height: "100%" }} alt="Macan" />
+                      <span className="cv_gallery_hover">
+                        <p>Photo Shoot</p>
+                        <h4>Macan</h4>
+                      </span>
+                    </div>
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+
+              <h2 className="photoshoots-heading" style={{marginTop:"30px",marginBottom:"30px"}}>Reels</h2>
+              <ReelDisplay reels={reels} />
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/component/ReelDisplay.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ReelDisplay.css"; // import the CSS file
+
+const ReelDisplay = ({ reels }) => {
+  return (
+    <div className="reel-container">
+      {reels.map((reel, index) => (
+        <div key={index} className="reel">
+          <video controls width="100%">
+            <source src={reel} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ReelDisplay;
